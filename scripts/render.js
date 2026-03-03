258da55c9a0acbe11e2a274505b2bf43aed6e146
Ivan Castellanos <ivanca@gmail.com>
2026-03-03 01:47:29 -0500

    twice as big

--- scripts/render.js
+++ scripts/render.js
@@ -152,7 +152,10 @@ export function gameRender() {
 		const angle = Math.atan2(-l.vel.y, l.vel.x);
 		// DrawRect(l.pos, vec2(0.3, 0.3), rgb(1, 1, 0.3));
 		const bulletColor = l.healing ? rgb(0.15, 1, 0.45) : WHITE;
-		const bulletSize = l.healing ? vec2(0.5, 0.5) : vec2(0.3, 0.3);
+		let bulletSize = l.healing ? vec2(0.5, 0.5) : vec2(0.3, 0.3);
+		if (isTouchDevice) {
+			bulletSize = bulletSize.scale(2);
+		}
 		drawTile(l.pos, bulletSize, tile(0, vec2(24, 24), imgs.bullet), bulletColor, angle);
 	}
 
